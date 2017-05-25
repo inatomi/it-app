bbf32370741aa61545e86de8f6e2435159f8478a
inatomi <inatomi@itrade.co.jp>
2017-05-25 17:19:51 +0900

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CloginComponent } from './clogin/clogin.component';
+import { CmainComponent } from './cmain/cmain.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CloginComponent
+    CloginComponent,
+    CmainComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MaterialModule,
     BrowserModule,
     FormsModule,
     HttpModule
