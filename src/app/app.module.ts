1925245e50d390292cf657c11f2bd03e255d8410
inatomi <inatomi@itrade.co.jp>
2017-05-25 16:19:33 +0900

    clogin add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CloginComponent } from './clogin/clogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CloginComponent
   ],
   imports: [
     BrowserModule,
